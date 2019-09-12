1f89ecdead418660ca109ac9020f50d9b3a6df97
Ashok Modi <btmash@gmail.com>
2019-09-12 14:20:25 -0700

    Add note controller

--- webroot/app/Http/Controllers/NoteController.php
+++ webroot/app/Http/Controllers/NoteController.php
@@ -2,9 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use App\Note;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class NoteController extends Controller
 {
-    //
+  //
+  public function list()
+  {
+    $user = Auth::user();
+    return $user->notes()->trainings;
+  }
+
+  public function create(Request $request)
+  {
+    $user = Auth::user();
+    $request->validate([
+      'title' => ['required', 'string', 'max:255'],
+      'email' => ['required', 'string', 'max:1000'],
+    ]);
+
+    $note = new Note([
+      'title' => $request->get('title'),
+      'note' => $request->get('note'),
+    ]);
+    $user->notes()->save($note);
+
+  }
+
 }
--- webroot/database/migrations/2019_09_12_202855_create_notes_table.php
+++ webroot/database/migrations/2019_09_12_202855_create_notes_table.php
@@ -15,6 +15,7 @@ class CreateNotesTable extends Migration
     {
         Schema::create('notes', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('name');
             $table->text('note');
             $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users');
